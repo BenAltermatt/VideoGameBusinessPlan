dfbb79d6a4698087582e9454ea3fbf96e615fb34
BenAltermatt <ben.altermatt20@gmail.com>
2023-03-10 23:19:36 -0500

    Added comment read in

--- Scripts/Comment.gd
+++ Scripts/Comment.gd
@@ -1,10 +1,12 @@
+# as of now you can't put new lines in comments or responses.
+# I'll hopefully fix that later
 
 # This class represents a single comment that can be displayed
 # within the game
-tool 
 extends EditorScript
 # These are all the things we can get out of a single 
 # comment in a game
+
 class_name Comment
 
 var user : String = "NO_NAME"
@@ -17,29 +19,119 @@ var responses = []
 
 
 # I want to be able to parse a comment out of a string from a file
-func _init(block):
+func _init(block=null):
+	assert(block !=  null, "Need a block to make a comment. Got null.")
+	var lines = block.split('\n')
+	
+	# we are going to handle the top line first as it includes most of our info
+	_read_args(lines[0])
+	_read_comm(lines[0])
+	
+	# Let's go through all of our attribute lines now
+	for i in range(1, len(lines)):
+		_read_attr(lines[i])
+
+func __init__(block:String):
 	# it might be easiest to split these into lines
 	var lines = block.split('\n')
 	
-	lines[0]
+	# we are going to handle the top line first as it includes most of our info
+	_read_args(lines[0])
+	_read_comm(lines[0])
 	
-	user = "NO_NAME"
-	pfp = "NO_FILE"
-	text = "NO_TEXT"
-	good_thresh = 0
-	end_a_thresh = 0
-	responses = []
+	# Let's go through all of our attribute lines now
+	for i in range(1, len(lines)):
+		_read_attr(lines[i])
+
+	
+
+func _read_args(top_line:String):
+	var top_args = top_line.split(" ")
+	
+	# the first arg should be (num,
+	var p_buf = top_args[0].right(1)
+	good_thresh = int(p_buf.left(len(p_buf) - 1))
+	
+	#second should be num,
+	p_buf = top_args[1]
+	end_a_thresh = int(p_buf.left(len(p_buf) - 1))
+	
+	# third should be num)
+	p_buf = top_args[2]
+	time_thresh = int(p_buf.left(len(p_buf) - 1))
+
+func _read_comm(top_line:String):
+	var start = top_line.find("\"")
+	var end = top_line.rfind("\"")
+	text = top_line.substr(start + 1, end - start - 1)
+	
+# these should have a preluding - in front of them
+func _read_attr(body_line:String):
+	# cut off the dash	
+	var line_cont = body_line.substr(body_line.find("-") + 1)
+
+	var substrs = line_cont.split(":")
+	
+	# the leftmost one is the identifier
+	var ident = substrs[0].strip_edges().to_lower()
+
+	var vals = ""
+	var x_fils = false
+	for val_ind in range(1, len(substrs)):
+		vals += substrs[val_ind]
+		if x_fils:
+			vals += ":"
+		x_fils = true
+	
+	vals = vals.strip_edges()
+	
+	match ident:
+		"user" : 
+			_read_user(vals)
+		"pfp" : 
+			_read_pfp(vals)
+		"cmt" : 
+			responses.append(Response.new(vals))
+		_ : 
+			assert(false, "Illegal comment attribute specified: %s" % ident)
+
+func _read_user(content:String):
+	var start = content.find("\"")
+	var end = content.rfind("\"")
+	user = content.substr(start + 1, end - start - 1)
+
+func _read_pfp(content:String):
+	var start = content.find("\"")
+	var end = content.rfind("\"")
+	pfp = content.substr(start + 1, end - start - 1)
+
+func print_comment():
+	print("(good_thresh, end_a_thresh, time_thresh) are (%d, %d, %d)" % [good_thresh, end_a_thresh, time_thresh])
+	print("Comment: %s" % text)
+	print("Username: %s" % user)
+	print("Pfp file: %s" % pfp)
+	for resp in responses:
+		print("Response:")
+		print("\t%s" % resp.text)
+		print("\t(good_change, end_a_change): (%d, %d)" % [resp.good_change, resp.end_a_change])
 
 class Response:
 	var text = "EMPTY_RESPONSE"
 	var good_change = 0
 	var end_a_change = 0
 
-	func _init(options):
-		text = "EMPTY_RESPONSE"
-		good_change = 0
-		end_a_change = 0
+	func _init(res_contents=null):
+		assert(res_contents != null, "Need contents for a response, got null.")
 
+		# first lets get the text set up
+		var start = res_contents.find("\"")
+		var end = res_contents.rfind("\"")
+		text = res_contents.substr(start + 1, end - start - 1)
 
-func _run():
-	print("AHAHAHHAHA")
+		# now we want to get the good and bad changes
+		var c_params = res_contents.substr(end + 1).strip_edges().right(1)
+		var val_strs = c_params.left(len(c_params) - 1).split(",")
+
+		good_change = int(val_strs[0])
+		end_a_change = int(val_strs[1])
+	
--- Scripts/ReadComments.gd
+++ Scripts/ReadComments.gd
@@ -1,4 +1,7 @@
-extends Node
+# it should be noted that these files cannot have semicolons or newlines within content of comments
+
+tool 
+extends EditorScript
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -6,26 +9,52 @@ extends Node
 const COMMENT_TEXT_PATH = "res://Scripts/comments.txt"
 const COMMENT_OBJECT_PATH = "res://Scripts/Comment.gd"
 
-
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	var file = File.new()
-	file.open(COMMENT_PATH, File.READ)
-	var content = file.get_as_text()
-	file.close()
+# func _ready():
 
-	Comment = load(COMMENT_OBJECT_PATH)
+	# var Comment = load(COMMENT_OBJECT_PATH)
+	# var file = File.new()
+	# file.open(COMMENT_TEXT_PATH, File.READ)
+	# var content = file.get_as_text()
+	# file.close()
 
-	comments = [] # this is where the comments are stored
+	# var comments = [] # this is where the comments are stored
 
-	# now we want to initialize the comments from our file
-	var blocks = content.split(";")
-	for block in blocks:
-		# the comment file should parse the text we pass in 
-		# as a single "comment" block
-		comments.append(Comment.new(block.strip()))
+	# # now we want to initialize the comments from our file
+	# var blocks = content.split(";")
+	# for block in blocks:
+	# 	# the comment file should parse the text we pass in 
+	# 	# as a single "comment" block
+	# 	comments.append(Comment.new(block.strip()))
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _run():
+	var Comment = load(COMMENT_OBJECT_PATH)
+	var file = File.new()
+	file.open(COMMENT_TEXT_PATH, File.READ)
+	var content = file.get_as_text()
+	file.close()
+
+	var blocks = content.split(";")
+	var comments = []
+
+	for block in blocks:
+		if len(block.strip_edges()) > 0:
+			comments.append(Comment.new(block.strip_edges()))
+
+	for comment in comments:
+		comment.print_comment()
+		print()
+
+
+# 	var input = "(10, 20, 30) \"Piss ass\"\n - User: \"Asshole\"\n - Pfp: \"pissass.png\"\n - Cmt: \"Certified dickhead\" (11, -3)"
+
+# 	var new_com = Comment.new(input)
+
+# 	new_com.print_comment()
