a2a9954a4e4710fe9d179f0770647a994dc05794
riomyoung <56449510+riomyoung@users.noreply.github.com>
2023-03-16 18:06:31 -0400

    Added a single video watch page (static))
    -Added a fonts folder for fonts
    -Added a "VideoSmall" scene for small video reccomendations on the side of the video playing
    -The current video pauses and plays
    -You can't click on recommended videos yet

---
+++ VideoPlayer.gd
@@ -0,0 +1,33 @@
+extends VideoPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+var currently_playing = true
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	print( "video clicked")
+	var currently_paused = is_paused()
+	if (currently_paused):
+		#stop()
+		
+		paused = false
+	else:
+		#play()
+		paused = true
+	var currently_playing = is_playing()
+	if (!currently_playing):
+		play()
+	pass # Replace with function body.
