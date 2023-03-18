07f8644f5b0671809981677f7993197b7c5659ab
Jamie-Sloop <jamiesloop@gmail.com>
2023-03-18 17:44:42 -0400

    working upload page

---
+++ Scripts/UploadManager.gd
@@ -0,0 +1,71 @@
+extends Control
+class_name UploadManager
+
+var _allVideos = []
+var _uploadOptionTemplate = load("res://Prefabs/UploadOptionTemplate.tscn")
+var _selectedVideo: UploadableVideoResource
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_allVideos = loadVideosFromFolder("res://UploadableVideos/")
+	_openPage(0, 0, 0)
+	# warning-ignore:return_value_discarded
+	get_node("VBoxContainer/MarginContainer/UploadButton").connect("pressed", self, "_uploadClicked")
+	# warning-ignore:return_value_discarded
+	get_tree().get_root().connect("size_changed", self, "_handleResize")
+
+func _openPage(x: float, y: float, day: int):
+	_selectedVideo = null
+	_delete_children(get_node("VBoxContainer/Panel/GridContainer"))
+	var todaysVideos = _filterVideos(x, y, day)
+	for i in range(todaysVideos.size()):
+		var template = _uploadOptionTemplate.instance()
+		get_node("VBoxContainer/Panel/GridContainer").add_child((template))
+		template._setUp(todaysVideos[i], self)
+
+func _filterVideos(x: float, y: float, day: int) -> Array:
+	var result = []
+	for i in range(_allVideos.size()):
+		if _allVideos[i].xMin > x:
+			continue
+		if _allVideos[i].xMax < x:
+			continue
+		if _allVideos[i].yMin > y:
+			continue
+		if _allVideos[i].yMax < y:
+			continue
+		if _allVideos[i].dayMin > day:
+			continue
+		if _allVideos[i].dayMax < day:
+			continue
+		result.append(_allVideos[i])
+	return result
+
+func _videoSelected(video: UploadableVideoResource):
+	_selectedVideo = video
+
+func _uploadClicked():
+	if _selectedVideo == null:
+		print("No video selected.")
+		return
+	print("Uploading video: " + _selectedVideo.videoName)
+
+func loadVideosFromFolder(path: String) -> Array:
+	var dir = Directory.new()
+	var result = []
+	dir.open(path)
+	dir.list_dir_begin()
+	var fileName = dir.get_next()
+	while fileName != "":
+		if not fileName.begins_with("."):
+			result.append(load(path + fileName))
+		fileName = dir.get_next()
+	return result
+
+static func _delete_children(node):
+	for n in node.get_children():
+		node.remove_child(n)
+		n.queue_free()
+
+func _handleResize():
+	get_node("VBoxContainer/Panel/GridContainer").columns = OS.get_window_size().x / 64
