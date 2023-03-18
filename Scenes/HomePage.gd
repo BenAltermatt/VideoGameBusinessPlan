537395e38ebc54646de1b776deb62a5a971d6932
riomyoung <56449510+riomyoung@users.noreply.github.com>
2023-03-18 18:36:00 -0400

    Working Main Page, connected to Single View Page

---
+++ Scenes/HomePage.gd
@@ -0,0 +1,83 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var videos = RecVideos.videos
+var num_videos = 6
+var video_path = "res://VideoFiles/"
+var thumbnail_path = "res://GameTextureAssets/"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_up()
+	pass # Replace with function body.
+	
+func set_up():
+	videos = RecVideos.videos
+	print( "setting videos")
+	for n in range(0, num_videos):
+		print(videos[n].title)
+		var rec_vid_node = get_node("Video" + str(n + 1) )
+		var rec_vid = videos[n]
+		
+		rec_vid_node.get_node("Title").set_text(rec_vid.title)
+		rec_vid_node.get_node("User").set_text(rec_vid.user)
+		var image = rec_vid_node.get_node("Thumbnail")
+		var texture = load( thumbnail_path + rec_vid.thumbnail_fname )
+		if (texture != null ):
+			image.texture = texture
+
+
+# when button pressed 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func open_single_view(index):
+	#var SINGLE_VID_PATH = "res://Scripts/SingleVideoWatch.gd"
+	#var singleVid = load(SINGLE_VID_PATH).new()
+	RecVideos.single_vid = true
+	RecVideos.cur_vid_index = index
+	#singleVid.set_video_current(index)
+	
+	
+
+func _on_Button1_pressed():
+	print("button1")
+	open_single_view(0)
+	pass # Replace with function body.
+
+
+func _on_Button2_pressed():
+	print("button2")
+	open_single_view(1)
+	pass # Replace with function body.
+
+
+func _on_Button3_pressed():
+	print("button3")
+	open_single_view(2)
+	pass # Replace with function body.
+
+
+func _on_Button4_pressed():
+	print("button4")
+	open_single_view(3)
+	pass # Replace with function body.
+
+
+func _on_Button5_pressed():
+	print("button5")
+	open_single_view(4)
+	pass # Replace with function body.
+
+
+func _on_Button6_pressed():
+	print("button6")
+	open_single_view(5)
+	pass # Replace with function body.
+
--- Scripts/YouTube.gd
+++ Scripts/YouTube.gd
@@ -2,15 +2,30 @@ extends Tabs
 
 
 # Declare member variables here. Examples:
-# var a = 2
+var single_vid = RecVideos.single_vid
+var multiple_vid = RecVideos.multiple_vid
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node("SingleWatchPage").hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	single_vid = RecVideos.single_vid
+	multiple_vid = RecVideos.multiple_vid
+	if (single_vid):
+		var singleVidNode = get_node("SingleWatchPage")
+		singleVidNode.set_video_current(RecVideos.cur_vid_index)
+		singleVidNode.show()
+		RecVideos.single_vid = false
+	if (multiple_vid):
+		print("CHANGED TO MULTIPLE")
+		var singleVidNode = get_node("HomePage")
+		singleVidNode.set_up()
+		RecVideos.multiple_vid = false
+		
+	pass
