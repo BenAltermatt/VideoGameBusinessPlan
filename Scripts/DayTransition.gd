d17d5fb3b4f9ee904bd44812db4917fe6f49b058
ben <ben.altermatt20@gmail.com>
2023-03-19 07:18:32 -0400

    Finished integrating everything and added a night cycle. Everything is fragile and dirty, but can easily be cleaned up.

---
+++ Scripts/DayTransition.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var elapsed
+var changed
+var s_day
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# this is  instantiated with the old day
+	elapsed = 0
+	changed = false
+	s_day = GameManager.curDay
+	
+	get_node("ColorRect/RichTextLabel").text = ("Day %d" % (s_day - 1))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	elapsed += delta
+	
+	if elapsed > 1 and not changed:
+		get_node("ColorRect/RichTextLabel").text = ("Day %d" % s_day)
+		changed = true
+	
