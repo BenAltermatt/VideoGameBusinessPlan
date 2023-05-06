89977b6de7e308cb3d2c9b85432fb62366e07d11
ben <ben.altermatt20@gmail.com>
2023-05-06 00:16:35 -0400

    Removed more superfluous files, reorganized some dependencies.

--- Scenes/HomePage.gd
+++ Scenes/HomePage.gd
@@ -19,15 +19,10 @@ func set_up():
 	videos = GameManager.cur_watches
 	num_videos = min(6, len(GameManager.cur_watches))
 	
-	#print( "setting videos")
 	for n in range(0, num_videos):
-		#print(videos[n].title)
 		var rec_vid_node = get_node("Video" + str(n + 1) )
 		rec_vid_node.visible = true
 		var rec_vid = GameManager.cur_watches[n]
-		#print(rec_vid.title)
-		#print(rec_vid.user)
-		#print(rec_vid.thumbnail_fname)
 		rec_vid_node.get_node("Title").set_text(rec_vid.title)
 		rec_vid_node.get_node("User").set_text(rec_vid.user)
 		var image = rec_vid_node.get_node("Thumbnail")
@@ -53,37 +48,25 @@ func open_single_view(index):
 	
 
 func _on_Button1_pressed():
-	#print("button1")
 	open_single_view(0)
-	pass # Replace with function body.
 
 
 func _on_Button2_pressed():
-	#print("button2")
 	open_single_view(1)
-	pass # Replace with function body.
 
 
 func _on_Button3_pressed():
-	#print("button3")
 	open_single_view(2)
-	pass # Replace with function body.
 
 
 func _on_Button4_pressed():
-	#print("button4")
 	open_single_view(3)
-	pass # Replace with function body.
 
 
 func _on_Button5_pressed():
-	#print("button5")
 	open_single_view(4)
-	pass # Replace with function body.
 
 
 func _on_Button6_pressed():
-	#print("button6")
 	open_single_view(5)
-	pass # Replace with function body.
 
