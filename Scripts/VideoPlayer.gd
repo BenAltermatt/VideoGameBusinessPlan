47ab5fc028cd025f6a195eec98ac539b99e14506
ben <ben.altermatt20@gmail.com>
2023-05-05 23:39:40 -0400

    Removed all redundant scripts and comments

--- Scripts/VideoPlayer.gd
+++ Scripts/VideoPlayer.gd
@@ -1,50 +1,23 @@
 extends VideoPlayer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-#var currently_playing = true
 var path_to_vid = "res://big-buck-bunny_trailer.webm"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	
-	#stream = load(path_to_vid)
-	#play()
-	#print("done loading")
-
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Button_pressed():
-	#print( "video clicked")
 	var currently_paused = is_paused()
 	if (currently_paused):
-		#stop()
-		
 		paused = false
 	else:
-		#play()
 		paused = true
 	var currently_playing = is_playing()
 	if (!currently_playing):
 		play()
-	pass # Replace with function body.
 
 
 func _on_Youtube_pressed():
 	stop()
-	pass # Replace with function body.
 
 
 func _on_Rewind_pressed():
 	stream_position = 0
 	set_stream_position(0)
-	#print("its rewind time")
-	pass # Replace with function body.
